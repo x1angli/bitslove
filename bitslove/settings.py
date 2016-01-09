38ee367c32ef896cd93001ab7d9b4abd0a24bb57
quezixiong <quezixiong@meiyuxiuxiu.com>
2016-01-09 15:14:13 +0800

    move static and templates to bitslove/static

--- bitslove/settings.py
+++ bitslove/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bitslove.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'static')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,5 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 AUTH_USER_MODEL = 'blwebapp.User'
--- blwebapp/templates/blwebapp/example.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by harley on 1/9/16.
- */
